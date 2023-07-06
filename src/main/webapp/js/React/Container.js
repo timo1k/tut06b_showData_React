77d444302c85ad607cd0f34bed9c7dbbfde43f8d
Timothy <timkhumpan@gmail.com>
2023-07-06 19:33:26 -0400

    update Container Component

--- src/main/webapp/js/React/Container.js
+++ src/main/webapp/js/React/Container.js
@@ -9,6 +9,23 @@ function Container() {
       <div className="center">
         <div className="inner">
           <h2>Center</h2>
+          {/* create a component that creates a list of Posts */}
+          <Posts
+            imageSrc={
+              "https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png"
+            }
+          ></Posts>
+          <Posts
+            imageSrc={
+              "https://m.media-amazon.com/images/M/MV5BMGM2MzA5YzYtODc0Ni00ZjU4LWI4ZmUtZGJjNGU0ODY1MGJkXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_FMjpg_UX1000_.jpg"
+            }
+          ></Posts>
+          <Posts></Posts>
+          <Posts></Posts>
+          <Posts></Posts>
+          <Posts></Posts>
+          <Posts></Posts>
+          <Posts></Posts>
         </div>
       </div>
       <div className="right">
