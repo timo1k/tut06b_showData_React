95931d2817d0b25e87cb1451737a32fbd7adcc83
Timothy <timkhumpan@gmail.com>
2023-07-05 23:02:39 -0400

    init feed componenet

---
+++ src/main/webapp/js/React/Container.js
@@ -0,0 +1,21 @@
+function Container() {
+  return (
+    <div className="container">
+      <div className="left">
+        <div className="inner">
+          <h2>Left</h2>
+        </div>
+      </div>
+      <div className="center">
+        <div className="inner">
+          <h2>Center</h2>
+        </div>
+      </div>
+      <div className="right">
+        <div className="inner">
+          <h2>Right</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
