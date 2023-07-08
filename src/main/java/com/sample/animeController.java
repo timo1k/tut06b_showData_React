5b90c5a7ac7d537adf9ce95dd1d4d0d6ce57522a
Timothy <timkhumpan@gmail.com>
2023-07-07 21:02:43 -0400

    update method and endpoint for anime

--- src/main/java/com/sample/animeController.java
+++ src/main/java/com/sample/animeController.java
@@ -4,6 +4,7 @@ package com.sample;
 import model.anime.*;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dbUtils.*;
@@ -13,16 +14,31 @@ import view.animeView;
 public class animeController {
 
   @RequestMapping(value = "/anime/getAll", produces = "application/json")
-    public String allUsers() {
+  public String allUsers() {
+    StringDataList list = new StringDataList(); // dbError empty, list empty
+    DbConn dbc = new DbConn();
+    list = animeView.getAllAnimes(dbc);
 
-        StringDataList list = new StringDataList(); // dbError empty, list empty
-        DbConn dbc = new DbConn();
-        list = animeView.getAllAnimes(dbc);
+    dbc.close(); // EVERY code path that opens a db connection must close it
+                 // (or else you have a database connection leak).
 
-        dbc.close(); // EVERY code path that opens a db connection must close it
-                     // (or else you have a database connection leak).
+    return Json.toJson(list); // convert sdl obj to JSON Format and return that.
+  }
 
-        return Json.toJson(list); // convert sdl obj to JSON Format and return that.
-    }
+  @RequestMapping(value = "/anime/findById", params = {"animeId"}, produces = "application/json")
+  public String getById(@RequestParam("animeId") String animeId) {
+      StringData sd = new StringData();
+      if (animeId == null) {
+          sd.errorMsg = "Error: URL must be anime/findById?animeId=xx " +
+                  "where xx is the animeId of the desired anime record.";
+      } else {
+          DbConn dbc = new DbConn();
+          sd = DbMods.findById(dbc, animeId);
+          dbc.close(); // EVERY code path that opens a db connection must close it
+          // (or else you have a database connection leak).
+      }
+      return Json.toJson(sd);
+  }
+
   
 }
--- src/main/java/model/anime/DbMods.java
+++ src/main/java/model/anime/DbMods.java
@@ -5,6 +5,44 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class DbMods {
+  public static StringData findById(DbConn dbc, String id) {
+
+    // The find API needs to represent three cases: found web_user, not found, db error. 
+    model.anime.StringData sd = new model.anime.StringData();
+    try {
+        String sql = "SELECT anime_id, anime_name, anime_japanese_name, anime_img, watch_date, anime_rating, anime_desc, anime.web_user_id, web_user.user_email "
+                + "FROM anime, web_user "
+                + "Where anime.web_user_id = web_user.web_user_id "
+                + "AND anime_id = ?";
+
+        PreparedStatement stmt = dbc.getConn().prepareStatement(sql);
+
+        // Encode the id (that the user typed in) into the select statement, into the first (and only) ? 
+        stmt.setString(1, id);
+
+        ResultSet results = stmt.executeQuery();
+        if (results.next()) { // id is unique, one or zero records expected in result set
+
+            // plainInteger returns integer converted to string with no commas.
+            sd.animeName = Format.fmtString(results.getObject("anime_name"));
+            sd.animeId = Format.fmtInteger(results.getObject("anime_id"));
+            sd.animeJapaneseName = Format.fmtString(results.getObject("anime_japanese_name"));
+            sd.animeImg = Format.fmtString(results.getObject("anime_img"));
+            sd.watchDate = Format.fmtDate(results.getObject("watch_date"));
+            sd.animeRating = Format.fmtInteger(results.getObject("anime_rating"));
+            sd.animeDesc = Format.fmtString(results.getObject("anime_desc"));
+            sd.webUserId = Format.fmtInteger(results.getObject("web_user_id"));
+            sd.userEmail = Format.fmtString(results.getObject("user_email"));
+        } else {
+            sd.errorMsg = "Anime Id Not Found.";
+        }
+        results.close();
+        stmt.close();
+    } catch (Exception e) {
+        sd.errorMsg = "Exception thrown in DbMods.findById(): " + e.getMessage();
+    }
+    return sd;
+  } // findById
         
 
 } // class
