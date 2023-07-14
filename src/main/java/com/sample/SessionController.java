71c3b9330ea169394f180bef9ee9af8afad0f57c
Timothy <timkhumpan@gmail.com>
2023-07-14 12:27:59 +0700

    init session controller

--- src/main/java/com/sample/SessionController.java
+++ src/main/java/com/sample/SessionController.java
@@ -1,6 +1,7 @@
 package com.sample;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.*;
@@ -24,6 +25,71 @@ public class SessionController {
             System.out.println("session/invalidate controller error: " + e.getMessage());
             sd.errorMsg += ". " + e.getMessage();
         }
+
+        return Json.toJson(sd);
+    }
+
+    @RequestMapping(value = "/session/read", produces = "application/json")
+    public String readController(HttpServletRequest request) {
+
+        HttpSession session = request.getSession();
+        StringData sd = new StringData();
+
+        try {
+            sd = (StringData) session.getAttribute("loggedOnUser");
+            if (sd != null) {
+                sd.errorMsg = "Above data was read from the session";
+            } else {
+                sd = new StringData();
+                sd.errorMsg = "Sorry but there is no logged in user";
+            }
+        } catch (Exception e) {
+            System.out.println("session/read controller error: " + e.getMessage());
+            sd.errorMsg += ". " + e.getMessage();
+        }
+        return Json.toJson(sd);
+    }
+
+
+    // sd.webUserId = Format.fmtInteger(results.getObject("web_user_id"));
+    //             sd.userEmail = Format.fmtString(results.getObject("user_email"));
+    //             sd.userPassword = Format.fmtString(results.getObject("user_password"));
+    //             sd.userImage = Format.fmtString(results.getObject("image"));
+    //             sd.birthday = Format.fmtDate(results.getObject("birthday"));
+    //             sd.membershipFee = Format.fmtDollar(results.getObject("membership_fee"));
+    //             sd.userRoleId = Format.fmtInteger(results.getObject("web_user.user_role_id"));
+    //             sd.userRoleType = Format.fmtString(results.getObject("user_role_type"));
+    //             sdl.add(sd);
+
+
+    @RequestMapping(value = "/session/loginSession", params = {"userEmail", "userPass" }, produces = "application/json")
+    public String loginSession(HttpServletRequest request, @RequestParam("userEmail") String userEmail, @RequestParam("userPassword") String userPass) {
+      HttpSession session = request.getSession();
+        StringData sd = new StringData();
+
+        if ((userEmail == null) || (userPass == null)) {
+          sd.errorMsg = "Cannot search for user: 'email' and 'password' must be supplied";
+        } else {
+          DbConn dbc = new DbConn();
+          sd.errorMsg = dbc.getErr();
+          
+          if (sd.errorMsg.length() == 0) {
+              System.out.println("*** Ready to call findUser");
+              sd = DbMods.findUser(dbc, userEmail, userPass);
+              
+              if ((sd != null) && (sd.webUserId != "")) {
+                  session.setAttribute("loggedOnUser", sd); // write object to JSP session object
+              }
+              else{
+                  sd = new StringData();
+                  sd.errorMsg = "Credentials not found, invalidating session"; 
+                  session.invalidate();
+                  
+              }
+          }
+          dbc.close();
+      }
+        sd.errorMsg = "Above data was written to the session";
 
         return Json.toJson(sd);
     }
--- src/main/java/model/webUser/DbMods.java
+++ src/main/java/model/webUser/DbMods.java
@@ -157,4 +157,44 @@ public class DbMods {
         return sd;
     } // getById
 
+    public static StringData findUser(DbConn dbc, String userEmail, String userPassword) {
+
+        StringData sd = new StringData();
+
+        try {
+            String sql = "SELECT web_user_id, user_email, user_password, membership_fee, birthday, image, "
+                    + "web_user.user_role_id, user_role_type "
+                    + "FROM web_user, user_role WHERE web_user.user_role_id = user_role.user_role_id "
+                    + "AND user_email = ? AND user_password = ?";
+
+            PreparedStatement stmt = dbc.getConn().prepareStatement(sql);
+
+            stmt.setString(1, userEmail); // 1 means the first  ? in the SQL
+            stmt.setString(2, userPassword); // 2 means the second ? in the SQL 
+
+            ResultSet results = stmt.executeQuery();
+            if (results.next()) { //Web User Found 
+
+                // plainInteger returns integer converted to string with no commas.
+                sd.webUserId = Format.fmtInteger(results.getObject("web_user_id"));
+                sd.userEmail = Format.fmtString(results.getObject("user_email"));
+                sd.userPassword = Format.fmtString(results.getObject("user_password"));
+                sd.userImage = Format.fmtString(results.getObject("image"));
+                sd.birthday = Format.fmtDate(results.getObject("birthday"));
+                sd.membershipFee = Format.fmtDollar(results.getObject("membership_fee"));
+                sd.userRoleId = Format.fmtInteger(results.getObject("web_user.user_role_id"));
+                sd.userRoleType = Format.fmtString(results.getObject("user_role_type"));
+                
+
+            } else {
+                sd.errorMsg = "Web User Not Found.";  //not found
+            }
+            results.close();
+            stmt.close();
+        } catch (Exception e) {
+            sd.errorMsg = "Exception thrown in DbMods.findUser(): " + e.getMessage(); //error
+        }
+        return sd;
+
+    } // findUser
 }
